7cd05e77ab5770489a1e2564c0f074538d655517
akryptonian <ritchie.dennis.2000@gmail.com>
2025-06-08 17:22:21 +0530

    Mainloop template

--- lc3.c
+++ lc3.c
@@ -61,3 +61,93 @@ enum
   OP_LEA, // load effective address
   OP_TRAP // execute trap
 }
+
+int main(int argc, const char* argv[])
+{
+
+  // Load arguments
+
+  if (argc < 2)
+  {
+    // Show usage string
+    printf("lc3 [image-file1] ...\n");
+    exit(2);
+  }
+
+  for (int j = 1; j < argc; ++j)
+  {
+    if (!read_image(argv[j]))
+    {
+      printf("failed to load image: %s\n", argv[j]);
+    }
+  }
+
+  // Setup
+
+  // Since exactly one condition flag should be set at any given time, set the Z flag
+
+  reg[R_COND] = FL_ZRO;
+
+  // Set the PC to starting position
+  // 0x3000 is the default
+
+  enum { PC_START = 0x3000};
+  reg[R_PC] = PC_START;
+
+  int running = 1;
+  while(running)
+  {
+    // Fetch
+    uint16_t instr = mem_read(reg[R_PC]++);
+    uint16_t op = instr >> 12;
+
+    switch(op)
+    {
+      case OP_ADD:
+      break;
+      
+      case OP_AND:
+      break;
+
+      case OP_NOT:
+      break;
+
+      case OP_BR:
+      break;
+
+      case OP_JMP:
+      break;
+
+      case OP_JSR:
+      break;
+
+      case OP_LD:
+      break;
+
+      case OP_LDI:
+      break;
+
+      case OP_LDR:
+      break;
+
+      case OP_LEA:
+      break;
+    
+      case OP_ST:
+      break;
+
+      case OP_STI:
+      break;
+
+      case OP_TRAP:
+      break;
+
+      case OP_RES:
+      case OP_RTI:
+      default:
+        // Bad Opcode
+        break;
+    }
+  } 
+  // Shutdown
+}
