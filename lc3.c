22fe5ce295d32daa7b587e97fbed9225533ad9c3
akryptonian <ritchie.dennis.2000@gmail.com>
2025-06-08 16:38:39 +0530

    Opcodes

--- lc3.c
+++ lc3.c
@@ -16,5 +16,24 @@ enum
   R_COUNT
 };
 
-unint16_t reg[R_COUNT];
+uint16_t reg[R_COUNT];
 
+enum
+{
+  OP_BR = 0, // branch
+  OP_ADD, // add
+  OP_LD, // load
+  OP_ST, // store
+  OP_JSR, // jump register
+  OP_AND, // bitwise and
+  OP_LDR, // load register
+  OP_STR, // store register
+  OP_RTI, // unused
+  OP_NOT, // bitwise not
+  OP_LDI, // load indirect
+  OP_STI, // store indirect
+  OP_JMP, // jump
+  OP_RES, // reserved (unused)
+  OP_LEA, // load effective address
+  OP_TRAP // execute trap
+}
