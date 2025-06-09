14fedcb85cfa44f217458bc241fc1da1d799bb46
akryptonian <ritchie.dennis.2000@gmail.com>
2025-06-10 00:41:08 +0530

    Minor bug fixes

--- trash/lc3.c
+++ trash/lc3.c
@@ -11,54 +11,6 @@
 
 #define MEMORY_MAX (1 << 16)
 
-// Registers
-
-enum
-{
-  R_R0 = 0,
-  R_R1,
-  R_R2,
-  R_R3,
-  R_R4,
-  R_R5,
-  R_R6,
-  R_R7,
-  R_PC, // Program counter
-  R_COND,
-  R_COUNT
-};
-
-// Condition flags
-
-enum
-{
-  FL_POS = 1 << 0, // P
-  FL_ZRO = 1 << 1, // Z
-  FL_NEG = 1 << 2, // N
-};
-
-// Opcodes
-
-enum
-{
-  OP_BR = 0, // branch
-  OP_ADD, // add
-  OP_LD, // load
-  OP_ST, // store
-  OP_JSR, // jump register
-  OP_AND, // bitwise and
-  OP_LDR, // load register
-  OP_STR, // store register
-  OP_RTI, // unused
-  OP_NOT, // bitwise not
-  OP_LDI, // load indirect
-  OP_STI, // store indirect
-  OP_JMP, // jump
-  OP_RES, // reserved (unused)
-  OP_LEA, // load effective address
-  OP_TRAP // execute trap
-};
-
 uint16_t memory[MEMORY_MAX]; // 65_536 locations
 uint16_t reg[R_COUNT];
 
@@ -66,8 +18,8 @@ uint16_t reg[R_COUNT];
 
 enum
 {
-  MR_KBSR = 0xFE00, // Keyboard status
-  MR_KBDR = 0xFE02, // Keyboard data
+  MR_KBSR = 0xFE00 // Keyboard status
+  MR_KBDR = 0xFE02 // Keyboard data
 };
 
 // TRAP Codes
@@ -80,7 +32,7 @@ enum
   TRAP_IN = 0X23, // Get character from keyboard, echoed onto the terminal
   TRAP_PUTSP = 0X24, // Output a byte string
   TRAP_HALT = 0X25, // Halt the program
-};
+}
 
 // Input Buffering
 
@@ -113,7 +65,7 @@ uint16_t check_key()
 
 // Handle Interrupt
 
-void handle_interrupt(int signal)
+void handle_iterrupt(int signal)
 {
   restore_input_buffering();
   printf("\n");
@@ -213,6 +165,55 @@ uint16_t mem_read(uint16_t address)
   return memory[address];
 }
 
+// Registers
+
+enum
+{
+  R_R0 = 0,
+  R_R1,
+  R_R2,
+  R_R3,
+  R_R4,
+  R_R5,
+  R_R6,
+  R_R7,
+  R_PC, // Program counter
+  R_COND,
+  R_COUNT
+};
+
+// Condition flags
+
+enum
+{
+  FL_POS = 1 << 0, // P
+  FL_ZRO = 1 << 1, // Z
+  FL_NEG = 1 << 2, // N
+};
+
+// Opcodes
+
+enum
+{
+  OP_BR = 0, // branch
+  OP_ADD, // add
+  OP_LD, // load
+  OP_ST, // store
+  OP_JSR, // jump register
+  OP_AND, // bitwise and
+  OP_LDR, // load register
+  OP_STR, // store register
+  OP_RTI, // unused
+  OP_NOT, // bitwise not
+  OP_LDI, // load indirect
+  OP_STI, // store indirect
+  OP_JMP, // jump
+  OP_RES, // reserved (unused)
+  OP_LEA, // load effective address
+  OP_TRAP // execute trap
+}
+
+
 int main(int argc, const char* argv[])
 {
 
@@ -283,14 +284,14 @@ int main(int argc, const char* argv[])
       
       case OP_AND:
       {
-        uint16_t r0 = (instr >> 9) & 0x7;
+        uint16_t r0 (instr >> 9) & 0x7;
         uint16_t r1 = (instr >> 9) & 0x7;
         uint16_t imm_flag = (instr >> 5) & 0x1;
         
         if(imm_flag)
         {
           uint16_t r2 = sign_extend(instr & 0x1F, 5);
-          reg[r0] = reg[r1] & imm_flag;
+          reg[r0] = reg[r1] & imm5;
         }
         else
         {
@@ -347,7 +348,7 @@ int main(int argc, const char* argv[])
         else // JSR
         {
           uint16_t pc_offset = instr & 0x7FF;
-          reg[R_PC] += sign_extend(pc_offset, 11);
+          reg[PC] += sign_extend(pc_offset, 11);
         }
       }
       break;
@@ -378,7 +379,7 @@ int main(int argc, const char* argv[])
       {
         uint16_t r0 = (instr >> 9) & 0x7;
         uint16_t r1 = (instr >> 6) & 0x7; //BaseR
-        uint16_t offset = sign_extend(instr & 0x3F, 6);
+        uint16_t offset = sign_extend((instr & 0x3F, 6);
         
         reg[r0] = mem_read(reg[r1] + offset);
         update_flags(r0);
@@ -443,7 +444,7 @@ int main(int argc, const char* argv[])
           case TRAP_PUTS:
           {
             // one char per word
-            uint16_t* c = memory + reg[R_R0];
+            uint16_t c = memory + reg[R_R0];
             while(*c)
             {
               putc((char)*c, stdout);
